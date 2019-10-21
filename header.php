2a0640b94b5ea077fb942caf0c5d99b31c99fc30
Francisco Mat <fmatelli@hsolmkt.com.br>
2019-10-21 08:15:32 -0300

    11 years late release

---
+++ header.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" <?php language_attributes(); ?>>
+
+<head profile="http://gmpg.org/xfn/11">
+<meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
+
+<title><?php bloginfo('name');
+if ( is_single() ) {
+	_e('&raquo; Blog Archive', 'kubrick');
+}
+wp_title(); ?></title>
+
+
+<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen" />
+<link rel="alternate" type="application/rss+xml" title="<?php printf(__('%s RSS Feed', 'kubrick'), get_bloginfo('name')); ?>" href="<?php bloginfo('rss2_url'); ?>" />
+<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
+<link href="/include/franciscomatelli_estilo.css" rel="stylesheet" type="text/css" />
+
+<?php wp_head(); ?>
+</head>
+<body>
+
+<?php
+#require_once("header.php");
+#require_once("menu.php");
+?>
+<div id="page">
